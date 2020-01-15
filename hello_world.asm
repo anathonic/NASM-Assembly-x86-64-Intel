e6899203b8eb3e2bfb2e41a0f56458a2e26ab201
tabacka <anitatabacka@gmail.com>
2020-01-15 14:23:54 +0100

    Update hello_world.asm

--- hello_world.asm
+++ hello_world.asm
@@ -5,13 +5,13 @@ global start
 section .text
 
 start:
-    mov     rax, 0x2000004 ; write
+    mov     rax, 0x2000004 ;write
     mov     rdi, 1 ; stdout
     mov     rsi, msg
     mov     rdx, msg.len
     syscall
 
-    mov     rax, 0x2000001 ; exit
+    mov     rax, 0x2000001 ;exit
     mov     rdi, 0
     syscall
 
