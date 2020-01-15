0948ce2331c665756f357b4c283cb77471c3df24
tabacka <anitatabacka@gmail.com>
2020-01-15 15:10:57 +0100

    Create max_from_3_registers.asm

---
+++ max_from_3_registers.asm
@@ -0,0 +1,22 @@
+global start
+section .text
+start:
+mov rbx, 128
+mov rcx, 64
+mov rdx, 32
+cmp rbx, rcx
+jle J1
+cmp rbx, rdx
+jle J1
+mov rax, rbx
+jmp end
+J1:
+cmp rcx, rdx
+jge J2
+mov rax, rdx
+jmp end
+J2:
+mov rax, rcx
+jmp end
+end:
+nop
